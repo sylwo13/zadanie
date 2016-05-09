3926517b27e9620af3e79f0b8f4b39056a4a621d
Sylwester <asd@asd>
2016-05-09 16:49:42 +0200

    Start zad 2

---
+++ zadanie1/zadanie_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
